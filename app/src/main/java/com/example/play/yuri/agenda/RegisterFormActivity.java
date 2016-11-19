7e05b20d1a7192f12738e9e6c8d5f466832535f2
Yuri Yonamine <yuriyonamine@hotmail.com>
2016-11-19 04:53:42 -0200

    Added toast message on save button click

--- app/src/main/java/com/example/play/yuri/agenda/RegisterFormActivity.java
+++ app/src/main/java/com/example/play/yuri/agenda/RegisterFormActivity.java
@@ -2,6 +2,9 @@ package com.example.play.yuri.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class RegisterFormActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class RegisterFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_form);
+
+        Button saveButton = (Button) findViewById(R.id.register_form_save);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(RegisterFormActivity.this,"Saved Successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
