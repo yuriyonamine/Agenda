8a8d6e88ea6be1a8a27d43733b7bef47529126d5
Yuri Yonamine <yuriyonamine@hotmail.com>
2016-11-19 02:34:55 -0200

    Changing static list content to ListAdapter

--- app/src/main/java/com/example/play/yuri/agenda/StudentListActivity.java
+++ app/src/main/java/com/example/play/yuri/agenda/StudentListActivity.java
@@ -2,6 +2,9 @@ package com.example.play.yuri.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class StudentListActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class StudentListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_list);
+
+        String[] students= {"Daniel", "John", "Valdir", "Heitor"};
+        ListView studentList = (ListView) findViewById(R.id.student_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, students);
+        studentList.setAdapter(adapter);
+
     }
 }
