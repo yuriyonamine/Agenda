11d475faa2b44e1e43a0de5aa4e931a35dae3e21
Yuri Yonamine <yuriyonamine@hotmail.com>
2016-11-19 22:33:49 -0200

    Added new OK button in menu

--- app/src/main/java/com/example/play/yuri/agenda/RegisterFormActivity.java
+++ app/src/main/java/com/example/play/yuri/agenda/RegisterFormActivity.java
@@ -2,6 +2,8 @@ package com.example.play.yuri.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,13 +14,24 @@ public class RegisterFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_form);
-
-        Button saveButton = (Button) findViewById(R.id.register_form_save);
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(RegisterFormActivity.this,"Saved Successfully", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.form_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.form_menu_ok:
+                Toast.makeText(RegisterFormActivity.this, "Saved Successfully", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
